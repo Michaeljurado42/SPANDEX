c23af17cff4d5205bcc69f5090ffc98cff704652
Michael Jurado <Michael.Jurado@gtri.gatech.edu>
2023-09-18 17:42:31 -0400

    Fixed bug in get_model_size.py. Need to include delays. Going to update yml metrics in next commit

--- baseline_solution/sdnn_delays/get_model_size.py
+++ baseline_solution/sdnn_delays/get_model_size.py
@@ -14,16 +14,20 @@ if __name__ == "__main__":
     trained_folder = args.trained_folder
     net = netx.hdf5.Network(trained_folder + '/network.net', sparsity_map=True)
     weights = [l.synapse.weights.get().toarray() for l in net.layers[1:4]] # convert from csr matrices
-    num_weights = sum([np.sum(w != 0) for w in weights])
     delays = [l.synapse.delays.get().toarray() for l in net.layers[1:3]]
+    num_weights = sum([np.sum(w != 0) for w in weights])
+    num_delays = sum([np.sum(d != 0) for d in delays])
     model_bits = sum([np.ceil(np.log2(np.abs(w).max())) * (w != 0).sum() for w in weights]) + sum([np.ceil(np.log2(np.abs(d)).max()) * (d != 0).sum() for d in delays])
     print('Model Size (KB) (Sparsity Estimate):', model_bits / 8 / 1024)
-    print("Total number of non-zero weights:",  num_weights)
+    print("Total number of non-zero params:",  num_weights + num_delays)
 
     # note I am pretty sure something is wrong with this estimate. The delays are 1d for example when they should be 2d
     print("Bamsumit model estimate") 
     net = netx.utils.NetDict(trained_folder + '/network.net')
     weights = [net['layer'][l]['weight'] for l in range(1, 4)]
     delays = [net['layer'][l]['delay'] for l in range(1, 3)]
+    num_weights = sum([np.prod(w.shape) for w in weights])
+    num_delays = sum([np.prod(d.shape) for d in delays])
     model_bits = sum([np.ceil(np.log2(np.abs(w).max())) * np.prod(w.shape) for w in weights]) + sum([np.ceil(np.log2(np.abs(d)).max()) * np.prod(d.shape) for d in delays])
     print('Model Size (KB):', model_bits / 8 / 1024)
+    print('Total number of weights', num_weights + num_delays)
