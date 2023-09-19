5a18df3133ede11fbf59cb4a157d78e20ae21fce
Michael Jurado <Michael.Jurado@gtri.gatech.edu>
2023-09-18 20:01:23 -0400

    Fixed incorrect estimate of number of parameters. Was not counting delays

--- baseline_solution/sdnn_delays/get_model_size.py
+++ baseline_solution/sdnn_delays/get_model_size.py
@@ -15,14 +15,14 @@ if __name__ == "__main__":
     net = netx.hdf5.Network(trained_folder + '/network.net', sparsity_map=True)
     weights = [l.synapse.weights.get().toarray() for l in net.layers[1:4]] # convert from csr matrices
     delays = [l.synapse.delays.get().toarray() for l in net.layers[1:3]]
-    num_weights = sum([np.sum(w != 0) for w in weights])
-    num_delays = sum([np.sum(d != 0) for d in delays])
+    nonzero_num_weights = sum([np.sum(w != 0) for w in weights])
+    nonzero_num_delays = sum([np.sum(d != 0) for d in delays])
     model_bits = sum([np.ceil(np.log2(np.abs(w).max())) * (w != 0).sum() for w in weights]) + sum([np.ceil(np.log2(np.abs(d)).max()) * (d != 0).sum() for d in delays])
     print('Model Size (KB) (Sparsity Estimate):', model_bits / 8 / 1024)
-    print("Total number of non-zero params:",  num_weights + num_delays)
-
-    # note I am pretty sure something is wrong with this estimate. The delays are 1d for example when they should be 2d
-    print("Bamsumit model estimate") 
+    print("Total number of non-zero params:",  nonzero_num_weights + nonzero_num_delays)
+    
+    # note I am pretty sure something is wrong with this code. The delays are 1d for example when they should be 2d
+    print("Bamsumit model estimate\n") 
     net = netx.utils.NetDict(trained_folder + '/network.net')
     weights = [net['layer'][l]['weight'] for l in range(1, 4)]
     delays = [net['layer'][l]['delay'] for l in range(1, 3)]
@@ -31,3 +31,13 @@ if __name__ == "__main__":
     model_bits = sum([np.ceil(np.log2(np.abs(w).max())) * np.prod(w.shape) for w in weights]) + sum([np.ceil(np.log2(np.abs(d)).max()) * np.prod(d.shape) for d in delays])
     print('Model Size (KB):', model_bits / 8 / 1024)
     print('Total number of weights', num_weights + num_delays)
+
+    # Note to Judges: 
+
+    # The percentage of non zero weights (Overall Sparsity) is about 5% lower
+    # for lava models created through netx than the pytorch version. 
+    # During Track 2 I will diagnose why this is
+    print("netX calculated Synaptic Sparsity", nonzero_num_weights/num_weights)
+
+
+
